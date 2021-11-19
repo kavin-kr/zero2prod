546b8f57d9fcd6d9d65402a69325cb7c9c093c41
kavin <kavin.kr25@gmail.com>
2021-11-19 13:12:12 +0530

    chap3 - 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    zero2prod::run(address)?.await
 }
