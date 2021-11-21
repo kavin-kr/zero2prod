ee8ee09c2df3763d288119f9d96ef74789efed5a
kavin <kavin.kr25@gmail.com>
2021-11-21 22:07:52 +0530

    chap6

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,14 +1,24 @@
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use std::convert::{TryFrom, TryInto};
 use uuid::Uuid;
-
-use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     name: String,
     email: String,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
 }
 
 #[tracing::instrument(
@@ -20,17 +30,12 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
         Err(e) => {
-            tracing::error!("Name validation failed: {:?}", e);
+            tracing::error!("Invalid parameter values: {:?}", e);
             return HttpResponse::BadRequest().finish();
         }
-    };
-
-    let new_subscriber = NewSubscriber {
-        name,
-        email: form.0.email,
     };
 
     match insert_subscriber(&pool, &new_subscriber).await {
@@ -56,7 +61,7 @@ async fn insert_subscriber(
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
